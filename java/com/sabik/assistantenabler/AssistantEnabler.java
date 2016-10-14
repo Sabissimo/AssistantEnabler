203d377149ed713db036d6387e6ba34ae72b3573
Sabissimo <sabanadze@gmail.com>
2016-10-15 02:26:00 +0400

    Merge from latest AndroidN-ify commits by Maxr1998
    1. Enable for all languages, regardless of support from Google
    2. OK Google always enabled (since settings activity can't be accessed for now)

--- java/com/sabik/assistantenabler/AssistantEnabler.java
+++ java/com/sabik/assistantenabler/AssistantEnabler.java
@@ -8,15 +8,16 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static de.robv.android.xposed.XposedBridge.log;
-import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 public class AssistantEnabler implements IXposedHookLoadPackage {
 
 	private static final String PACKAGE_NAME = "com.google.android.googlequicksearchbox";
-	private static final String ASSISTANT_PACKAGE = "com.google.android.apps.gsa.assistant";
+	private static final String GSA_PACKAGE = "com.google.android.apps.gsa";
+	private static final String ASSISTANT_PACKAGE = GSA_PACKAGE + ".assistant";
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
@@ -29,7 +30,11 @@ public class AssistantEnabler implements IXposedHookLoadPackage {
 			findAndHookConstructor(a, findClass("com.google.android.apps.gsa.search.core.config.GsaConfigFlags", lpparam.classLoader), SharedPreferences.class, new XC_MethodHook() {
 				@Override
 				protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-					callMethod(param.thisObject, "aG", true);
+					SharedPreferences prefs = (SharedPreferences) getObjectField(param.thisObject, "bhX");
+					prefs.edit().putBoolean("key_opa_eligible", true)
+							.putBoolean("opa_enabled", true)
+							.putBoolean("opa_hotword_enabled", true)
+							.putBoolean("opa_hotword_transition_seen", true).apply();
 				}
 			});
 
@@ -47,6 +52,13 @@ public class AssistantEnabler implements IXposedHookLoadPackage {
 				}
 			});
 
+			findAndHookMethod(a, "oZ", new XC_MethodReplacement() {
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+					return true;
+				}
+			});
+
 			findAndHookMethod(a, "pb", new XC_MethodReplacement() {
 				@Override
 				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
@@ -54,24 +66,12 @@ public class AssistantEnabler implements IXposedHookLoadPackage {
 				}
 			});
 
-			findAndHookMethod(a, "pc", new XC_MethodReplacement() {
+			findAndHookMethod(GSA_PACKAGE + ".shared.util.c", lpparam.classLoader, "v", String.class, boolean.class, new XC_MethodHook() {
 				@Override
-				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-					return true;
-				}
-			});
-
-			findAndHookMethod(a, "oY", new XC_MethodReplacement() {
-				@Override
-				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-					return true;
-				}
-			});
-
-			findAndHookMethod(a, "oZ", new XC_MethodReplacement() {
-				@Override
-				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-					return true;
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					if (param.args[0].toString().equals("ro.opa.eligible_device")) {
+						param.setResult(true);
+					}
 				}
 			});
 		} catch (Throwable t) {
