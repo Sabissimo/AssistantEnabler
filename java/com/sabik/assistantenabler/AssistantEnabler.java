a6128ccdb1c1cf9126d2d12e40361868f1a62e12
Sabissimo <sabanadze@gmail.com>
2016-11-07 02:32:10 +0400

    Version 2.0
    * Added GUI
    * Added ability to enable/disable assistant without rebooting
    * Added ability to enable Google Now in unsupported countries
    * Donation button ;)

--- java/com/sabik/assistantenabler/AssistantEnabler.java
+++ java/com/sabik/assistantenabler/AssistantEnabler.java
@@ -1,13 +1,21 @@
 package com.sabik.assistantenabler;
 
+//import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
+//import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -17,51 +25,68 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
-public class AssistantEnabler implements IXposedHookLoadPackage {
+public class AssistantEnabler implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
-	private static final String GOOGLE_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
-	private static final String GSA_PACKAGE = "com.google.android.apps.gsa";
-	private static final String ASSISTANT_PACKAGE = GSA_PACKAGE + ".assistant";
-	private String googleBHX;
-	private String googleAG;
-	private String googlePA;
-	private String googleOZ;
-	private String googlePB;
+    private XSharedPreferences prefs;
+    private static final String GOOGLE_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+    private static final String GSA_PACKAGE = "com.google.android.apps.gsa";
+    private static final String ASSISTANT_PACKAGE = GSA_PACKAGE + ".assistant";
+    private static final String TELEPHONY_CLASS = "android.telephony.TelephonyManager";
+    private static final List<String> NOW_PACKAGE_NAMES = new ArrayList<>(Arrays.asList("com.google.android.gms", "com.google.android.apps.maps"));
+    private String googleBHX;
+    private String googleAG;
+    private String googlePA;
+    private String googleOZ;
+    private String googlePB;
+
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+        prefs = new XSharedPreferences(AssistantEnabler.class.getPackage().getName(), "preferences");
+        prefs.makeWorldReadable();
+    }
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		if (GOOGLE_PACKAGE_NAME.equals(lpparam.packageName))
-		{
-			if(checkVersion(lpparam)) {
-				try {
-					Class assistantClass = findClass(ASSISTANT_PACKAGE + ".a.e", lpparam.classLoader);
-					Class gsaConfigFlagsClass = findClass("com.google.android.apps.gsa.search.core.config.GsaConfigFlags", lpparam.classLoader);
+        prefs.reload();
+        if (GOOGLE_PACKAGE_NAME.equals(lpparam.packageName) && prefs.getBoolean("assistantEnabled", true) && checkVersion(lpparam)) {
+            try {
+                Class assistantClass = findClass(ASSISTANT_PACKAGE + ".a.e", lpparam.classLoader);
+                Class gsaConfigFlagsClass = findClass("com.google.android.apps.gsa.search.core.config.GsaConfigFlags", lpparam.classLoader);
 
-					findAndHookConstructor(assistantClass, gsaConfigFlagsClass, SharedPreferences.class, assistantBHXHook);
-					findAndHookMethod(assistantClass, googleAG, boolean.class, assistantAGHook);
-					findAndHookMethod(assistantClass, googlePA, assistantPAHook);
-					findAndHookMethod(assistantClass, googleOZ, assistantOZHook);
-					findAndHookMethod(assistantClass, googlePB, assistantPBHook);
-					findAndHookMethod(GSA_PACKAGE + ".shared.util.c", lpparam.classLoader, "v", String.class, boolean.class, assistantGSAHook);
-				} catch (Throwable t) {
-					log(t);
-				}
-			}
-		}
-	}
+                findAndHookConstructor(assistantClass, gsaConfigFlagsClass, SharedPreferences.class, assistantBHXHook);
+                findAndHookMethod(assistantClass, googleAG, boolean.class, assistantAGHook);
+                findAndHookMethod(assistantClass, googlePA, assistantPAHook);
+                findAndHookMethod(assistantClass, googleOZ, assistantOZHook);
+                findAndHookMethod(assistantClass, googlePB, assistantPBHook);
+                findAndHookMethod(GSA_PACKAGE + ".shared.util.c", lpparam.classLoader, "v", String.class, boolean.class, assistantGSAHook);
+            } catch (Throwable t) {
+                log(t);
+            }
+        }
+        if (NOW_PACKAGE_NAMES.contains(lpparam.packageName) && prefs.getBoolean("googleNowEnabled", false)){
+            try{
+                findAndHookMethod(TELEPHONY_CLASS, lpparam.classLoader, "getSimOperator", nowOperatorCodeHook);
+                findAndHookMethod(TELEPHONY_CLASS, lpparam.classLoader, "getSimCountryIso", nowCountryISOHook);
+                findAndHookMethod(TELEPHONY_CLASS, lpparam.classLoader, "getSimOperatorName", nowOperatorNameHook);
+            }
+            catch (Throwable t){
+                log(t);
+            }
+        }
+    }
 
-	private Boolean checkVersion(LoadPackageParam lpparam) throws PackageManager.NameNotFoundException {
+    private Boolean checkVersion(LoadPackageParam lpparam) throws PackageManager.NameNotFoundException {
 
-		Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
-		Context context = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
-		String versionName = context.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
+        Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
+        Context context = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
+        String versionName = context.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
 
-		if (versionName.matches("6.6.*")) {
-			googleBHX = "bhX";
-			googleAG = "aG";
-			googlePA = "pa";
-			googleOZ = "oZ";
-			googlePB = "pb";
+        if (versionName.matches("6.6.*")) {
+            googleBHX = "bhX";
+            googleAG = "aG";
+            googlePA = "pa";
+            googleOZ = "oZ";
+            googlePB = "pb";
         } else if (versionName.matches("6.7.*")) {
             googleBHX = "biJ";
             googleAG = "aK";
@@ -75,60 +100,88 @@ public class AssistantEnabler implements IXposedHookLoadPackage {
             googleOZ = "pT";
             googlePB = "pV";
         } else {
-			return false;
-		}
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private XC_MethodHook assistantBHXHook = new XC_MethodHook() {
-		@Override
-		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-			SharedPreferences prefs = (SharedPreferences) getObjectField(param.thisObject, googleBHX);
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-				prefs.edit().putBoolean("key_opa_eligible", true)
-						.putBoolean("opa_enabled", true)
-						.putBoolean("opa_hotword_enabled", true)
-						.putBoolean("opa_hotword_transition_seen", true).apply();
-			}
-		}
-	};
+    private XC_MethodReplacement nowOperatorCodeHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+            return "310004";
+        }
+    };
 
-	private XC_MethodHook assistantAGHook = new XC_MethodHook() {
-		@Override
-		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-			param.args[0] = true;
-		}
-	};
+    private XC_MethodReplacement nowCountryISOHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+            return "us";
+        }
+    };
 
-	private XC_MethodReplacement assistantPAHook = new XC_MethodReplacement() {
-		@Override
-		protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-			return true;
-		}
-	};
+    private XC_MethodReplacement nowOperatorNameHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+            return "Verizon";
+        }
+    };
 
-	private XC_MethodReplacement assistantOZHook = new XC_MethodReplacement() {
-		@Override
-		protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-			return true;
-		}
-	};
+    private XC_MethodHook assistantBHXHook = new XC_MethodHook() {
+        @Override
+        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            boolean assistantEnabled = prefs.getBoolean("assistantEnabled", true);
+            SharedPreferences googlePrefs = (SharedPreferences) getObjectField(param.thisObject, googleBHX);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+                googlePrefs.edit().putBoolean("key_opa_eligible", assistantEnabled)
+                        .putBoolean("opa_enabled", assistantEnabled).apply();
+            }
+        }
+    };
 
-	private XC_MethodReplacement assistantPBHook = new XC_MethodReplacement() {
-		@Override
-		protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-			return true;
-		}
-	};
+    private XC_MethodHook assistantAGHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            boolean assistantEnabled = prefs.getBoolean("assistantEnabled", true);
+            param.args[0] = assistantEnabled;
+        }
+    };
 
-	private XC_MethodHook assistantGSAHook = new XC_MethodHook() {
-		@Override
-		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-			if (param.args[0].toString().equals("ro.opa.eligible_device")) {
-				param.setResult(true);
-			}
-		}
-	};
+    private XC_MethodReplacement assistantPAHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            return prefs.getBoolean("assistantEnabled", true);
+        }
+    };
+
+    private XC_MethodReplacement assistantOZHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            return prefs.getBoolean("assistantEnabled", true);
+        }
+    };
+
+    private XC_MethodReplacement assistantPBHook = new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            return prefs.getBoolean("assistantEnabled", true);
+        }
+    };
+
+    private XC_MethodHook assistantGSAHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            prefs.reload();
+            boolean assistantEnabled = prefs.getBoolean("assistantEnabled", true);
+            if (param.args[0].toString().equals("ro.opa.eligible_device")&&assistantEnabled) {
+                param.setResult(true);
+            }
+        }
+    };
 
 }
---
+++ java/com/sabik/assistantenabler/Settings.java
@@ -0,0 +1,87 @@
+package com.sabik.assistantenabler;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class Settings extends AppCompatActivity {
+
+    public Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        final SharedPreferences prefs = getSharedPreferences("preferences",MODE_WORLD_READABLE);
+        final SharedPreferences.Editor editor = prefs.edit();
+        context = getApplicationContext();
+        Switch assistantEnabled = (Switch) findViewById(R.id.assistantEnabled);
+        Switch googleNowEnabled = (Switch) findViewById(R.id.googleNowEnabled);
+        Switch hiddenIcon = (Switch) findViewById(R.id.hiddenIcon);
+        Button donateButton = (Button) findViewById(R.id.donateButton);
+
+        if (googleNowEnabled!=null) {
+            googleNowEnabled.setChecked(prefs.getBoolean("googleNowEnabled",true));
+            googleNowEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    editor.putBoolean("googleNowEnabled", isChecked);
+                    editor.apply();
+
+                }
+            });
+        }
+
+        if (assistantEnabled!=null) {
+            assistantEnabled.setChecked(prefs.getBoolean("assistantEnabled",true));
+            assistantEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    editor.putBoolean("assistantEnabled", isChecked);
+                    editor.apply();
+
+                }
+            });
+        }
+
+        if (hiddenIcon!=null) {
+            hiddenIcon.setChecked(prefs.getBoolean("hiddenIcon",false));
+            hiddenIcon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    PackageManager packageManager = context.getPackageManager();
+                    int state = isChecked ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                            : PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+                    String settings = BuildConfig.APPLICATION_ID + ".Preferences";
+                    ComponentName alias = new ComponentName(context, settings);
+                    packageManager.setComponentEnabledSetting(alias, state,
+                            PackageManager.DONT_KILL_APP);
+
+                    editor.putBoolean("hiddenIcon", isChecked);
+                    editor.apply();
+
+                }
+            });
+        }
+
+        if (donateButton!=null){
+            donateButton.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.me/Sabissimo")));
+                }
+            });
+        }
+
+    }
+}
